ddfd9aa072e5cf5a9a6015a0e7d2f7ceca5980c8
sugumaran-jesus <suguks33317@gmail.com>
2026-04-16 10:53:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import gdown
 import numpy as np
 from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model
@@ -6,15 +7,37 @@ from tensorflow.keras.preprocessing import image
 
 app = Flask(__name__)
 
-# Load model
-model = load_model("../model/model.h5")
-
-# Upload folder
 UPLOAD_FOLDER = "static/uploads/"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Class labels (IMPORTANT)
-class_names = os.listdir("../dataset/PlantVillage")
+# Download model from Google Drive
+MODEL_PATH = "model.h5"
+if not os.path.exists(MODEL_PATH):
+    gdown.download(
+        "https://drive.google.com/uc?id=1JqrQB23cZVjXMnxEQGntzLhHzEGxXWL0",
+        MODEL_PATH, quiet=False
+    )
+
+model = load_model(MODEL_PATH)
+
+# Class names
+class_names = sorted([
+    'Pepper__bell__Bacterial_spot',
+    'Pepper__bell__healthy',
+    'Potato__Early_blight',
+    'Potato__healthy',
+    'Potato__Late_blight',
+    'Tomato__Target_Spot',
+    'Tomato__Tomato_mosaic_virus',
+    'Tomato__Tomato_YellowLeaf__Curl_Virus',
+    'Tomato_Bacterial_spot',
+    'Tomato_Early_blight',
+    'Tomato_healthy',
+    'Tomato_Late_blight',
+    'Tomato_Leaf_Mold',
+    'Tomato_Septoria_leaf_spot',
+    'Tomato_Spider_mites_Two_spotted_spider_mite'
+])
 
 @app.route('/')
 def index():
@@ -29,12 +52,10 @@ def predict():
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(filepath)
 
-    # Preprocess image
     img = image.load_img(filepath, target_size=(128, 128))
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Prediction
     prediction = model.predict(img_array)
     result = class_names[np.argmax(prediction)]
 
